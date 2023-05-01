2b6d8a83d8fdb3705da10193b64c23a37b1dcf64
30531 <305310835@qq.com>
2023-05-01 10:49:33 +0800

    labuladong的算法小抄完整版 第零章（必读系列），第一章（DP）部分。

---
+++ dp/剑指019.py
@@ -0,0 +1,46 @@
+"""
+请实现一个函数用来匹配包含'. '和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。
+在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
+"""
+
+
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        def matches(i: int, j: int) -> bool:
+            if i == 0:
+                return False
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
+
+        f = [[False] * (n + 1) for _ in range(m + 1)]
+        f[0][0] = True
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    f[i][j] |= f[i][j - 2]
+                    if matches(i, j - 1):
+                        f[i][j] |= f[i - 1][j]
+                else:
+                    if matches(i, j):
+                        f[i][j] |= f[i - 1][j - 1]
+        return f[m][n]
+
+
+def test(self, s: str, p: str, ans: bool):
+    if self.isMatch(s, p) == ans:
+        print('√')
+    else:
+        print('×')
+
+
+if __name__ == '__main__':
+    soul = Solution()
+    soul.test('aa', 'a', False)
+    soul.test('aa', 'a*', True)
+    soul.test('ab', '.*', True)
+    soul.test('aab', 'c*a*b*', True)
+    soul.test('mississippi', 'mis*is*p*', False)
