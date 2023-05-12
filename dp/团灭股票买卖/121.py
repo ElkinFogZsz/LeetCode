266cf6b7a8b9565f418ff7f171a953e73e2f504f
30531 <305310835@qq.com>
2023-05-12 17:07:24 +0800

    labuladong的算法小抄完整版 第零章（必读系列），第一章（DP）部分。

---
+++ dp/团灭股票买卖/121.py
@@ -0,0 +1,48 @@
+"""
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+示例 1：
+
+输入：[7,1,5,3,6,4]
+输出：5
+解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+示例 2：
+
+输入：prices = [7,6,4,3,1]
+输出：0
+解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+"""
+
+import numpy as np
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        # 第一维是日期,第二维是剩余购买次数，第三维是是否持有
+        dp = [[[0, 0] for j in range(2)] for i in range(1 + len(prices))]
+        k = 1
+        dp[1][k-1][1] = -prices[0]
+        dp[1][k][1] = -np.inf
+        # 第一天
+        for i in range(2, 1 + len(prices)):
+            for j in range(1):
+                dp[i][j][0] = max(dp[i - 1][j][1] + prices[i - 1], dp[i - 1][j][0])
+                dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i - 1])
+
+        return max(max(dp[len(prices)]))
+
+    def test(self, prices, ans):
+        if self.maxProfit(prices) == ans:
+            print('√')
+        else:
+            print('×')
+
+
+if __name__ == '__main__':
+    Solution().test([7, 1, 5, 3, 6, 4], 5)
+    Solution().test([7, 6, 4, 3, 1], 0)
+    Solution().test([3, 2, 6, 5, 0, 3], 4)
+    Solution().test([1, 2], 1)
